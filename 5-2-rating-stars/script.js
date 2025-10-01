f3c4d5bd0ff4dabc2b2a27acaed37ece818376e4
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2025-10-01 10:06:50 +0200

    Finish task 5-2

---
+++ 5-2-rating-stars/script.js
@@ -0,0 +1,19 @@
+const empty_path = "img/star_empty.jpg"
+const filled_path = "img/star_filled.jpg"
+
+for (let i = 1; i <= 5; i++) {
+    const new_star = document.createElement("img");
+    new_star.src = empty_path;
+    new_star.classList.add("star-" + i); 
+    new_star.addEventListener("click", rate);
+    document.querySelector(".star_menu").appendChild(new_star);
+}
+
+function rate(star) {
+    const Class = this.className;
+    const number = parseInt(Class.split("-")[1]);
+    for (let i = 1; i <= number; i++) {
+        const selected_stars = document.querySelector(".star-" + i);
+        selected_stars.src = filled_path;
+    }
+}
