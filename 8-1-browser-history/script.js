8157d430587d6e34e0655b97a6aebc4dd5661e20
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2025-10-02 15:57:26 +0200

    Finish task 8-1

---
+++ 8-1-browser-history/script.js
@@ -0,0 +1,24 @@
+const reduceBtn = document.getElementById("minus1");
+const addBtn = document.getElementById("plus1");
+const count = document.getElementById("counter");
+
+reduceBtn.addEventListener("click", decrease);
+addBtn.addEventListener("click", increase);
+
+let counter = 0;
+count.innerText = counter;
+
+const baseURL = 'http://localhost:3000/%5C8-1-browser-history/';
+let newURL = baseURL + counter;
+
+function decrease() {
+    counter--;
+    count.innerText = counter;
+    history.pushState({ page: counter }, "title", "?page=" + counter)
+}
+
+function increase() {
+    counter++;
+    count.innerText = counter;
+    history.pushState({ page: counter }, "title", "?page=" + counter)
+}
